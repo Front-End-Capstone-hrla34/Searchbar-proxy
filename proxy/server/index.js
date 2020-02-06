5f48c51b28f2e8f95f0dce85704c097249126620
Brian Price <brianprice88@gmail.com>
2020-02-06 15:34:31 -0800

    "Finished proxy server MVP - all services running on server."

--- proxy/server/index.js
+++ proxy/server/index.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const path = require('path');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('dev'));
 
